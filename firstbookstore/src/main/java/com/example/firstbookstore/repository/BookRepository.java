07b407491d852f39e11add2885fc0754636137d4
ChillyWolf <ayouuuuuub17@gmail.com>
2020-04-06 18:29:01 +0100

    Angular Loading data, Fonts imported, CORS problem resolved

--- firstbookstore/src/main/java/com/example/firstbookstore/repository/BookRepository.java
+++ firstbookstore/src/main/java/com/example/firstbookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.example.firstbookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.firstbookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Integer> {
 	
 
